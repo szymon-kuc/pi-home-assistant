8fe650d2bed12fb0effb3204e173168af78dcdfa
szymon <szymon.kuc@poczta.onet.pl>
2019-12-23 23:24:21 +0100

    add fuction, make code little bit cleaner

--- speach.py
+++ speach.py
@@ -5,24 +5,32 @@ rl = sr.Recognizer()
 r2 = sr.Recognizer()
 r3 = sr.Recognizer()
 
+def kill_process():
+    subprocess.Popen(["killall", "chrome"])
+
+def tasks(text):
+    pikachu = text.split()[0]
+    commmand = text.split()[1]
+    content = text.replace((pikachu + " " + commmand + " "), '')
+    pikachu = pikachu.lower()
+    commmand = commmand.lower()
+
+    if(pikachu == "pikachu"):
+        if(commmand == "play"):
+            kill_process()
+            subprocess.Popen(["node", "bot.js", content])
+        if(commmand == "stop"):
+            kill_process()
+
 while True:
-    with sr.Microphone(device_index=0) as source:
+    with sr.Microphone() as source:
         print('speak now')
         audio = r3.listen(source)
-
         try:
             get = r3.recognize_google(audio)
             print(get)
-            txt = ""
-            if(len(get.split()) > 2):
-                txt = get.split()[0] + " " + get.split()[1]
-                get = get.replace(txt + " ", '')
-                txt = txt.lower()
-            if(txt == "pikachu play"):
-                subprocess.Popen(["killall", "chrome"])
-                subprocess.Popen(["node", "bot.js", get])
-            if(get == "stop"):
-                subprocess.Popen(["killall", "chrome"])
+            if(len(get.split()) >= 2):
+                tasks(get)    
         except sr.UnknownValueError as e:
             print(e)
         except sr.RequestError as e:
