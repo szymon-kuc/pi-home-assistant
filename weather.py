27fe22880e6e1ecf0a35f6840b8c15a3c3dfafac
IronKnight525 <szymon.kuc@poczta.onet.pl>
2019-12-24 22:39:03 +0100

    weather err handle

--- weather.py
+++ weather.py
@@ -5,17 +5,20 @@ import urllib.request
 def get_weather(api, location_id):
     url = 'http://dataservice.accuweather.com/currentconditions/v1/%s?apikey=%s&details=true' % (location_id, api)
     print(url)
-    with urllib.request.urlopen(url) as url:
-        data = json.loads(url.read().decode())
-    return (data[0]['Temperature']['Metric']['Value'],
-            data[0]['RelativeHumidity'],
-            data[0]['Wind']['Direction']['Degrees'],
-            data[0]['Wind']['Speed']['Metric']['Value'],
-            data[0]['UVIndex'],
-            data[0]['CloudCover'],
-            data[0]['Pressure']['Metric']['Value'],
-            data[0]['Precip1hr']['Metric']['Value'],
-            data)
+    try:
+        with urllib.request.urlopen(url) as url:
+            data = json.loads(url.read().decode())
+        return (data[0]['Temperature']['Metric']['Value'],
+                data[0]['RelativeHumidity'],
+                data[0]['Wind']['Direction']['Degrees'],
+                data[0]['Wind']['Speed']['Metric']['Value'],
+                data[0]['UVIndex'],
+                data[0]['CloudCover'],
+                data[0]['Pressure']['Metric']['Value'],
+                data[0]['Precip1hr']['Metric']['Value'],
+                data)
+    except urllib.error.HTTPError as err:
+        return ([err] * 9)
 
 
 timestamp = time.time()
